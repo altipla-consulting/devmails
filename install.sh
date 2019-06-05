adc81d3da2c16337416763357ef50a7ecaa49af7
Ernesto Alejo <ernestokarim@gmail.com>
2019-06-05 16:01:58 +0200

    Add some infra files.

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -eu
+
+REPO_NAME=devmails
+
+URL=$(curl -sSL https://api.github.com/repos/altipla-consulting/$REPO_NAME/releases/latest | jq -r '.assets[].browser_download_url')
+echo "Downloading latest release from: $URL"
+curl $URL -sSL -o /usr/local/bin/$REPO_NAME || (
+  echo ""
+  echo " [!!!] This command needs sudo priviledge"
+  echo ""
+  exit 1
+)
+
+chmod +x,o+x /usr/local/bin/$REPO_NAME
+
+echo ""
+echo "Run the app with:"
+echo "    $REPO_NAME"
+echo ""
